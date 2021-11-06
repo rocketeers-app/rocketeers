c0ff7175f666a8d223f3deba783688701e7c6095
Mark van Eijk <mark@vaneijk.co>
2021-11-06 13:28:53 +0100

    wip

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,8 +8,8 @@
             <h2 class="block text-3xl font-extrabold text-green-400 sm:text-6xl">
                 Sites management for digital agencies and developers
             </h2>
-            <p class="max-w-xl mx-auto mt-4 text-base leading-normal text-white sm:text-xl">Always in control in every aspect of your sites and web apps using our SaaS control panel for server hosting, domains,<br>performance, uptime, metrics and more.</p>
             <p class="block m-4 text-base text-gray-600 sm:text-xl">Launching in 2022</p>
+            <p class="max-w-xl mx-auto mt-4 text-base leading-normal text-white sm:text-xl">Always in control in every aspect of your sites and web apps using our SaaS control panel for server hosting, domains,<br>performance, uptime, metrics and more.</p>
         </div>
         <div class="px-4">
             <form action="#" class="relative max-w-lg mx-auto mt-1 md:mt-4">
