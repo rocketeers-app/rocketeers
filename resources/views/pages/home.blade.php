5774685875ead62971fe04db57174cc0a555442e
Mark van Eijk <mark@vaneijk.co>
2021-11-06 13:19:42 +0100

    wip

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -14,8 +14,8 @@
         <div class="px-4">
             <form action="#" class="relative max-w-lg mx-auto mt-1 md:mt-4">
                 <label for="email" class="sr-only">Fill in your email address to get notified</label>
-                <input id="email" type="email" class="block w-full py-4 pl-5 text-sm sm:text-base text-gray-900 placeholder-gray-500 border border-transparent rounded-md shadow-sm pr-[180px] focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-green-600" placeholder="Leave your email address to stay updated">
-                <button type="submit" class="absolute top-0 right-0 px-5 py-4 text-sm font-medium text-green-900 bg-green-400 border border-transparent rounded-md rounded-l-none shadow sm:text-base hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-green-600 sm:px-10">Notify me</button>
+                <input id="email" type="email" class="block w-full py-4 pl-5 text-base text-gray-900 placeholder-gray-500 border border-transparent rounded-md shadow-sm pr-[180px] focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-green-600" placeholder="Leave your email address to stay updated">
+                <button type="submit" class="absolute top-0 right-0 px-5 py-4 text-base font-medium text-green-900 bg-green-400 border border-transparent rounded-md rounded-l-none shadow hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-green-600 sm:px-10">Notify me</button>
             </form>
         </div>
     </main>
