65fdb2dd661ef4927019e437274dc8fd49561499
Mark van Eijk <mark@vaneijk.co>
2022-08-15 23:24:43 +0200

    twitter cards

--- resources/views/layouts/partials/meta.blade.php
+++ resources/views/layouts/partials/meta.blade.php
@@ -3,12 +3,11 @@
 <meta charset="utf-8">
 <title>{{ $title }}</title>
 <link rel="canonical" href="{{ url()->current() }}">
-@if(isset($description))
-<meta name="description" content="{{ $description }}">
-<meta property="og:description" content="{{ $description }}">
+@if(isset(app()->view->getSections()['description']))
+<meta name="description" content="{{ app()->view->getSections()['description'] }}">
 @endif
-@if(isset($keywords))
-<meta name="keywords" content="{{ $keywords }}">
+@if(isset(app()->view->getSections()['keywords']))
+<meta name="keywords" content="{{ app()->view->getSections()['keywords'] }}">
 @endif
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -16,7 +15,11 @@
 <meta property="author" content="Mark van Eijk">
 <meta name="generator" content="Rocketeers">
 <meta name="robots" content="index,follow">
+<link rel="stylesheet" href="{{ mix('css/app.css') }}">
 <meta property="og:title" content="{{ $title }}">
+@if(isset(app()->view->getSections()['description']))
+<meta property="og:description" content="{{ app()->view->getSections()['description'] }}">
+@endif
 <meta property="og:type" content="website">
 <meta property="og:url" content="{{ url()->current() }}">
 <meta property="og:image" content="{!! url()->signedRoute('open-graph-image-file', compact('title')) !!}">
@@ -28,7 +31,14 @@
 @if(isset(app()->view->getSections()['updated_at']))
 <meta property="article:modified_time" content="{{ app()->view->getSections()['updated_at'] }}">
 @endif
-<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@rocketeersapp">
+<meta name="twitter:creator" content="@markvaneijk">
+<meta name="twitter:title" content="{{ $title }}">
+@if(isset(app()->view->getSections()['description']))
+<meta name="twitter:description" content="{{ app()->view->getSections()['description'] }}">
+@endif
+<meta name="twitter:image" content="{!! url()->signedRoute('open-graph-image-file', compact('title')) !!}">
 @env('production')
     <script src="https://cdn.usefathom.com/script.js" data-site="EXPMPOZO" defer></script>
 @endenv
