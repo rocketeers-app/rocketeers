b1e99570c6735f1bb87cecc0fe83049ae37fdb11
Mark van Eijk <mark@vaneijk.co>
2022-08-17 23:03:22 +0200

    wip

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Article;
 use App\Models\Feature;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\Tags\Url;
 
