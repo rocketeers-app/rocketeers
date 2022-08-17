9376f2ef3dc75f1d98aee3d4fd7e98f0e23593d3
Mark van Eijk <mark@vaneijk.co>
2022-08-18 00:13:18 +0200

    use png

--- app/Actions/GenerateOpenGraphImage.php
+++ app/Actions/GenerateOpenGraphImage.php
@@ -18,7 +18,7 @@ class GenerateOpenGraphImage
         }
 
         $title = request('title') ?: config('app.name');
-        $filename = str_slug($title).'.jpg';
+        $filename = str_slug($title).'.png';
 
         $html = view('social.open-graph-image', compact('title'));
 
@@ -37,15 +37,12 @@ class GenerateOpenGraphImage
         Browsershot::html($html)
             ->showBackground()
             ->windowSize(1200, 630)
-            ->setScreenshotType('jpeg', 100)
+            ->setScreenshotType('png')
             ->save($path);
     }
 
     public function getOpenGraphImageResponse($filename)
     {
-        return response(
-            Storage::disk('public')->get('social/open-graph/'.$filename), 200, [
-            'Content-Type' => 'image/jpeg',
-        ]);
+        return redirect('storage/social/open-graph/'.$filename);
     }
 }
