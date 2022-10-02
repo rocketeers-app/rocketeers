718abf94e0de7afa3a800fb9ce8f45a95452554e
Mark van Eijk <mark@vaneijk.co>
2022-10-02 22:40:15 +0200

    add laravel-static package

--- app/helpers.php
+++ app/helpers.php
@@ -1,9 +1,18 @@
 <?php
 
+use Vormkracht10\LaravelStatic\Console\Terminal;
+
 if (! function_exists('markdown')) {
     function markdown($markdown)
     {
         return app(Spatie\LaravelMarkdown\MarkdownRenderer::class)
             ->toHtml($markdown);
+    }
+}
+
+if (! function_exists('console')) {
+    function console()
+    {
+        return new Terminal;
     }
 }
