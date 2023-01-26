09fe358c7af1dfb1285633412e9ce292c2fd5510
Mark van Eijk <mark@vaneijk.co>
2023-01-26 15:48:49 +0100

    twitter card tags

--- resources/views/vendor/open-graph-image/components/metatags.blade.php
+++ resources/views/vendor/open-graph-image/components/metatags.blade.php
@@ -4,6 +4,10 @@
     @endif
 @endforeach
 
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@rocketeers_app" />
+<meta name="twitter:creator" content="@markvaneijk" />
+
 <meta property="og:image" content="{!! og($attributes) !!}">
 <meta property="og:image:type" content="image/{{ config('open-graph-image.image.extension') }}">
 <meta property="og:image:width" content="{{ config('open-graph-image.image.width') }}">
