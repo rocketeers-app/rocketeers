edfe57f42a630f7648efbf1b53f67c338ab2aa8f
Mark van Eijk <mark@vaneijk.co>
2023-12-03 12:24:58 +0100

    launching 2024

--- resources/views/pages/pricing.blade.php
+++ resources/views/pages/pricing.blade.php
@@ -3,7 +3,7 @@
 @section('main')
     <div class="container max-w-screen-md py-10 mx-auto">
         <div class="text-center">
-            <p class="block mb-2 text-base text-gray-500 sm:text-xl">Launching early 2023</p>
+            <p class="block mb-2 text-base text-gray-500 sm:text-xl">Launching in 2024</p>
             <h1 class="block mb-4 text-3xl font-extrabold text-emerald-400 sm:text-6xl">
                 Pricing
             </h1>
