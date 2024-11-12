cab4488d1d223322ad46160ea4b2231fb87d36c0
Mark van Eijk <mark@vaneijk.co>
2024-11-12 23:59:30 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,33 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\FeatureController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LandingPages\DevelopersController;
-use App\Http\Controllers\LandingPages\DigitalAgenciesController;
 use App\Http\Controllers\PricingController;
 use App\Http\Controllers\SitemapController;
 use App\Http\Controllers\SubscribeController;
-use Illuminate\Support\Facades\Route;
+use Vormkracht10\LaravelStatic\Middleware\StaticResponse;
+use App\Http\Controllers\LandingPages\DevelopersController;
+use App\Http\Controllers\LandingPages\DigitalAgenciesController;
 
 require 'redirects.php';
 
-Route::get('/', HomeController::class)->name('home');
-Route::get('pricing', PricingController::class)->name('pricing');
-Route::get('contact', ContactController::class)->name('contact');
-Route::post('subscribe', SubscribeController::class);
-Route::get('sitemap.xml', SitemapController::class);
+Route::middleware([StaticResponse::class])->group(function () {
+    Route::get('/', HomeController::class)->name('home');
+    Route::get('pricing', PricingController::class)->name('pricing');
+    Route::get('contact', ContactController::class)->name('contact');
+    Route::post('subscribe', SubscribeController::class);
+    Route::get('sitemap.xml', SitemapController::class);
 
-Route::get('developers', DevelopersController::class);
-Route::get('digital-agencies', DigitalAgenciesController::class);
+    Route::get('developers', DevelopersController::class);
+    Route::get('digital-agencies', DigitalAgenciesController::class);
 
-Route::get('features', [FeatureController::class, 'index'])->name('feature.index');
-Route::get('features/{feature}', [FeatureController::class, 'show'])->name('feature.show')->where('feature', '.*');
+    Route::get('features', [FeatureController::class, 'index'])->name('feature.index');
+    Route::get('features/{feature}', [FeatureController::class, 'show'])->name('feature.show')->where('feature', '.*');
 
-Route::feeds();
+    Route::feeds();
 
-Route::get('{slug?}', ArticleController::class)->name('knowledge')->where('slug', '.*');
+    Route::get('{slug?}', ArticleController::class)->name('knowledge')->where('slug', '.*');
+});
