6a434b25007f2e16bfcb23ae21e803da5e3df069
Mark van Eijk <mark@ux.nl>
2026-02-04 20:05:05 +0100

    wip

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,7 +8,7 @@ class ArticleController extends Controller
 {
     public function __invoke($slug)
     {
-        $categories = Article::pluck('category')
+        $categories = Article::pluck("category")
             ->unique()
             ->sort()
             ->filter()
@@ -16,44 +16,50 @@ class ArticleController extends Controller
                 return [str_slug($category) => $category];
             });
 
-        $articles = Article::published()
-            ->when(null !== $category = $categories[$slug] ?? null, function ($query) use ($category) {
-                $query->where('category', $category);
-            })
-            ->orderByDesc('published_at')
-            ->get();
+        $articles = Article::query()
+            ->when(
+                null !== ($category = $categories[$slug] ?? null),
+                fn($query) => $query->where("category", $category),
+            )
+            ->get()
+            ->sortByDesc("published_at");
 
         if (is_null($category)) {
             $articles = $articles->take(5);
         }
 
-        if (! is_null($category) || $slug == 'knowledge') {
-            return view('articles.index', compact('category', 'categories', 'articles'));
+        if (!is_null($category) || $slug == "knowledge") {
+            return view(
+                "articles.index",
+                compact("category", "categories", "articles"),
+            );
         }
 
-        $article = Article::published()
-            ->where('slug', $slug)
-            ->firstOrFail();
+        $article = Article::published()->where("slug", $slug)->firstOrFail();
 
         $relatedArticlesList = Article::published()
-            ->where('category', $article->category)
-            ->where('slug', '!=', $article->slug)
-            ->orderBy('published_at')
+            ->where("category", $article->category)
+            ->where("slug", "!=", $article->slug)
+            ->orderBy("published_at")
             ->get();
 
         $relatedArticles = collect();
 
         if ($relatedArticlesList->count() > 2) {
-            $relatedArticles = $relatedArticlesList->filter(function ($relatedArticle) use ($article) {
-                return $relatedArticle->published_at <= $article->published_at;
-            })
+            $relatedArticles = $relatedArticlesList
+                ->filter(function ($relatedArticle) use ($article) {
+                    return $relatedArticle->published_at <=
+                        $article->published_at;
+                })
                 ->take(2);
         }
 
         if ($relatedArticles->count() < 2) {
-            $relatedArticles = $relatedArticles->merge($relatedArticlesList->take(2 - $relatedArticles->count()));
+            $relatedArticles = $relatedArticles->merge(
+                $relatedArticlesList->take(2 - $relatedArticles->count()),
+            );
         }
 
-        return view('articles.show', compact('article', 'relatedArticles'));
+        return view("articles.show", compact("article", "relatedArticles"));
     }
 }
